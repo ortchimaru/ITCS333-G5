fc654dcdfb9e7cead568c18a471a9119dd00bd98
ortchimaru <abdulla.jalal223@gmail.com>
2025-05-13 15:45:32 +0300

    Course Notes Backend

--- CourseNotesPHP/get-note.php
+++ CourseNotesPHP/get-note.php
@@ -1,5 +1,8 @@
 <?php
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST");
+
 require_once 'db.php';
 
 if (!isset($_GET['id'])) {
